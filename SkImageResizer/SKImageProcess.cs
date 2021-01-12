b91d8aef6ab73de3f3e0ea1ec45b8174077c30f8
github-lym <69377506+github-lym@users.noreply.github.com>
2021-01-12 08:09:37 +0800

    v2_套用別人作法

--- SkImageResizer/SKImageProcess.cs
+++ SkImageResizer/SKImageProcess.cs
@@ -71,13 +71,24 @@ namespace SkImageResizer
                     var destinationWidth = (int)(sourceWidth * scale);
                     var destinationHeight = (int)(sourceHeight * scale);
 
-                    using var scaledBitmap = bitmap.Resize(
-                        new SKImageInfo(destinationWidth, destinationHeight),
-                        SKFilterQuality.High);
-                    using var scaledImage = SKImage.FromBitmap(scaledBitmap);
-                    using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
-                    using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
-                    data.SaveTo(s);
+                    // using var scaledBitmap = bitmap.Resize(
+                    //                                   new SKImageInfo(destinationWidth, destinationHeight),
+                    //                                   SKFilterQuality.High);
+                    // using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+                    // using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
+                    // using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
+                    // data.SaveTo(s);
+
+                    Task.Run(() =>
+                    {
+                        using var scaledBitmap = bitmap.Resize(
+                                  new SKImageInfo(destinationWidth, destinationHeight),
+                                  SKFilterQuality.High);
+                        using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+                        using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
+                        using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
+                        data.SaveTo(s);
+                    });
                 }));
 
             }
